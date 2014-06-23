ed1ba56eed2903a9288dee693052e757fb71615c
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-23 15:17:50 -0600

    Finished sub bonus of using objects

--- exercises/week2/js4-victims-and-volunteers/main.js
+++ exercises/week2/js4-victims-and-volunteers/main.js
@@ -1,39 +1,41 @@
-var numberOfVictims = prompt("How many disaster victims do you wish to enter?");
+var victims = {};
+victims.number = prompt("How many disaster victims do you wish to enter?");
 
-var nameVictim = [];
-var phoneVictim = [];
-var streetVictim = [];
+victims.name = [];
+victims.phone = [];
+victims.street = [];
 
-for(var i = 1; i <= numberOfVictims; i++) {
-	nameVictim[i] = prompt("Name of victim " + i);
-	phoneVictim[i] = prompt("Phone number of victim " + i);
-	streetVictim[i] = prompt("Street of victim " + i);
+for(var i = 1; i <= victims.number; i++) {
+	victims.name[i] = prompt("Name of victim " + i);
+	victims.phone[i] = prompt("Phone number of victim " + i);
+	victims.street[i] = prompt("Street of victim " + i);
 }
 
-var numberOfVolunteers = prompt("How many volunteers do you wish to enter?");
+var volunteers = {};
+volunteers.number = prompt("How many volunteers do you wish to enter?");
 
-var nameVolunteer = [];
-var phoneVolunteer = [];
-var streetVolunteer = [];
+volunteers.name = [];
+volunteers.phone = [];
+volunteers.street = [];
 
-for(var i = 1; i <= numberOfVolunteers; i++) {
-	nameVolunteer.push(prompt("Name of Volunteer" + i));
-	phoneVolunteer.push(prompt("Phone number of Volunteer" + i));
-	streetVolunteer.push(prompt("Street of Volunteer" + i));
+for(var i = 1; i <= volunteers.number; i++) {
+	volunteers.name.push(prompt("Name of Volunteer" + i));
+	volunteers.phone.push(prompt("Phone number of Volunteer" + i));
+	volunteers.street.push(prompt("Street of Volunteer" + i));
 }
 
-alert("Number of people in need: " + numberOfVictims + "\n" 
-	+ "Number of volunteers: " + numberOfVolunteers);
+alert("Number of people in need: " + victims.number + "\n" 
+	+ "Number of volunteers: " + volunteers.number);
 
 document.write("<h1>volunteers</h1><ul>");
-nameVolunteer.forEach(function(name) {
+volunteers.name.forEach(function(name) {
 	document.write("<li>" + name + "</li>");
 });
 document.write("</ul>");
 
 
 document.write("<h1>victims</h1><ul>");
-nameVictim.forEach(function(name) {
+victims.name.forEach(function(name) {
 	document.write("<li>" + name + "</li>");
 });
 document.write("</ul>");
