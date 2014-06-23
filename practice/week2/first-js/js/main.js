b53a1e350571bc7347f5af3bf8085e37f49777c2
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-23 11:22:53 -0600

    Started browser js basics

---
+++ practice/week2/first-js/js/main.js
@@ -0,0 +1,26 @@
+console.log("Hello Boulder!\n");
+
+var x;
+
+x = 5000;
+
+if(x < 1000) {
+	console.log("tiny");
+}
+else if (x > 1001) {
+	console.log("not so tiny")
+}
+else {
+	console.log("exactly 1000 or something else completely");
+}
+
+// Primitive types
+
+/*
+	string
+	number
+	boolean
+	null
+	undefined
+ */
+
