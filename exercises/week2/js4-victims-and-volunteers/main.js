31afead02f2d063b69527249a05519535a7c36d1
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-23 18:59:48 -0600

    Added form and ability to add victims and volunteers to a list.

--- exercises/week2/js4-victims-and-volunteers/main.js
+++ exercises/week2/js4-victims-and-volunteers/main.js
@@ -1,60 +1,11 @@
-var victims = [];
-var volunteers = [];
-var count = 0;
-var name = "";
-var phone = "";
-var street = "";
-
-while(confirm("Would you like to enter a victim?") === true) {
-	victims[count] = {
-		name: prompt("Name of victim"),
-		phone: prompt("Phone number of victim"),
-		street: prompt("Street of victim")
-	};
-	count++;
+function addVictim(form) {
+	var listItem = document.createElement("li")
+	listItem.innerHTML = "<li>" + form.victimName.value + "</li>";
+	document.getElementById("victims").appendChild(listItem);
 }
 
-count = 0;
-while(confirm("Would you like to enter a volunteer?") === true) {
-	volunteers[count] = {
-		name: prompt("Name of Volunteer"),
-		phone: prompt("Phone number of Volunteer"),
-		street: prompt("Street of Volunteer")
-	};
-	count++;
+function addVolunteer(form) {
+	var listItem = document.createElement("li")
+	listItem.innerHTML = "<li>" + form.volunteerName.value + "</li>";
+	document.getElementById("volunteers").appendChild(listItem);
 }
-
-document.write("<h1>Number of people in need: " + victims.length + "</h1>");
-victims.forEach(function(victim) {
-	document.write("<ul><li>" + victim.name + "</li>");
-	document.write("<li><ul><li>" + victim.phone + "</li>");
-	document.write("<li>" + victim.street + "</li></ul></ul>");
-});
-
-document.write("<h1>Number of volunteers: " + volunteers.length + "</h1>");
-volunteers.forEach(function(volunteer) {
-	document.write("<ul><li>" + volunteer.name + "</li>");
-	document.write("<li><ul><li>" + volunteer.phone + "</li>");
-	document.write("<li>" + volunteer.street + "</li></ul></ul>");
-});
-
-var victimName = prompt("Enter the name of a victim");
-victims.forEach(function(victim){
-	if(victimName === victim.name) {
-		street = victim.street;
-	}
-});
-
-var volunteersAvailable = [];
-volunteers.forEach(function(volunteer) {
-	if(street === volunteer.street) {
-		volunteersAvailable.push(volunteer)
-	}
-});
-
-document.write("<h2>Help for " + victimName + "</h2>");
-document.write("<ul>");
-volunteersAvailable.forEach(function(volunteer){
-	document.write("<li>" + volunteer.name + "</li>");
-});
-document.write("</ul>");
