64843b64c9e97727699d213b95eb6809dae86b54
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-24 11:07:17 -0600

    Finished bonus 3

--- exercises/week2/js4-victims-and-volunteers/main.js
+++ exercises/week2/js4-victims-and-volunteers/main.js
@@ -1,11 +1,24 @@
+var volunteers = [];
+
 function addVictim(form) {
 	var listItem = document.createElement("li")
-	listItem.innerHTML = "<li>" + form.victimName.value + "</li>";
+	listItem.innerHTML = "<li>" + form.victimName.value + "<button onclick='checkVictim(\"" + form.victimAddress.value + "\");'>Check</button></li>";
 	document.getElementById("victims").appendChild(listItem);
 }
 
 function addVolunteer(form) {
+	volunteers.push({name: form.volunteerName.value, phone: form.volunteerPhone.value, address: form.volunteerAddress.value});
 	var listItem = document.createElement("li")
 	listItem.innerHTML = "<li>" + form.volunteerName.value + "</li>";
 	document.getElementById("volunteers").appendChild(listItem);
+}
+
+function checkVictim(victimAddress) {
+	var message = "The following volunteers are available: \n ";
+	volunteers.forEach(function(volunteer) {
+		if(victimAddress.toLowerCase() === volunteer.address.toLowerCase()) {
+			message += volunteer.name + " - " + volunteer.phone + "\n ";
+		}
+	});
+	alert(message);
 }
