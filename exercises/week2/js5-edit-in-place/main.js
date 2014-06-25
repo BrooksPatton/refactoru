b90b8b2e260f1b2e591eda215a5a4e97f08d0dce
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-25 15:47:47 -0600

    Finished part 2 of JS5 - Edit in Place

--- exercises/week2/js5-edit-in-place/main.js
+++ exercises/week2/js5-edit-in-place/main.js
@@ -1,5 +1,48 @@
+var hideTextBlock = function(obj) {
+	$("#" + obj.id).hide();
+}
+
+var insertTextField = function(obj) {
+	var inputId = obj.id + "-input";
+	var textInput = "<textarea class='block input' id='" + inputId + "'></textarea>";
+	$("#" + obj.id).before(textInput);
+	$("#" + inputId).css({
+		"height": obj.height,
+		"width": obj.width
+	});
+	$("#" + inputId).text(obj.content);
+
+	$("#" + inputId).focus();
+
+	blurEvent(inputId);
+
+	return inputId;
+}
+
+var getOriginalId = function(inputId) {
+	var arr = inputId.split("-");
+	return arr[0];
+};
+
+var blurEvent = function(inputId) {
+	$("#" + inputId).blur(function() {
+		var content = $("#" + inputId).val();
+		var originalId = getOriginalId(inputId);
+		$("#" + inputId).remove();
+		$("#" + originalId).show();
+		$("#" + originalId).text(content);
+	});
+};
+
 $(document).on('ready', function() {
   $(".editable").click(function(element) {
-  	console.log(element.target.id);
-  })
+  	var clickedOnId = {
+  		id: element.target.id,
+  		height: $("#" + element.target.id).height(),
+  		content: $("#" + element.target.id).text(),
+  		width: $("#" + element.target.id).width(),
+  	};
+  	hideTextBlock(clickedOnId);
+  	insertTextField(clickedOnId);
+  });
 });
