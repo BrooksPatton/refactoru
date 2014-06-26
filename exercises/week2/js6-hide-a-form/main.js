2910b3a455d7161bacb0849308e1d69d557021a2
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-26 16:01:01 -0600

    Finished normal JS6 - Hide a Form

--- exercises/week2/js6-hide-a-form/main.js
+++ exercises/week2/js6-hide-a-form/main.js
@@ -10,7 +10,19 @@ var saveProfile = function(e) {
 	editProfile.books = $("#edit-books").val();
 	editProfile.jsLibraries = $("#edit-js-libraries").val();
 	loadProfile(editProfile);
+	switchToProfile();
 };
+
+var switchToEditProfile = function() {
+	populateEditProfileForm();
+	toggleDisplay(".edit-button");
+	toggleDisplay(".profile");
+	toggleDisplay(".edit-profile");
+};
+
+var cancelEdit = function() {
+	switchToProfile();
+}
 
 // Function declaration
 var loadProfile = function(profile) {
@@ -18,11 +30,32 @@ var loadProfile = function(profile) {
 	$("#bio").text(profile.bio);
 	$("#books").text(profile.books);
 	$("#js-libraries").text(profile.jsLibraries);
-}
+};
+
+var toggleDisplay = function(element) {
+	$(element).toggle();
+};
+
+var switchToProfile = function() {
+	toggleDisplay(".edit-button");
+	toggleDisplay(".profile");
+	toggleDisplay(".edit-profile");
+};
+
+var populateEditProfileForm = function() {
+	$("#edit-name").val($("#name").text());
+	$("#edit-bio").val($("#bio").text());
+	$("#edit-books").val($("#books").text());
+	$("#edit-js-libraries").val($("#js-libraries").text());
+};
 
 // Event handlers
 $(document).on('ready', function() {
   
 	$("#edit-submit").on("click", saveProfile);
+
+	$("#edit-profile").on("click", switchToEditProfile);
+
+	$("#edit-cancel").on("click", cancelEdit);
 
 });
