4d6371a762156cdee933b1a9965156f54cd6700f
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-26 08:42:54 -0600

    Finished part 2 of JS4.1 intro to jQuery

--- exercises/week2/js4.2-intro-to-jquery/main.js
+++ exercises/week2/js4.2-intro-to-jquery/main.js
@@ -10,4 +10,26 @@ $(document).on('ready', function() {
 	$("#list").on("click", function() {
 		$("body").append('<ul><li>1</li><li>2</li><li>3</li></ul>');
 	});
+
+	$(".paragraph").on("mouseenter", function(element) {
+		var id = "#" + element.target.id;
+		$(id).addClass('c-pink');
+	})
+
+	$(".paragraph").on('mouseleave', function(event) {
+		var id = "#" + event.target.id;
+		$(id).removeClass('c-pink');
+	});
+
+	$(".heading").on("mouseenter", function(event) {
+		$(".heading").append("!");
+	});
+
+	$("a").on('click', function(event) {
+		var id = "#" + event.target.id;
+		if(!confirm("Do you want to navigate away from this page?")) {
+			$(id).remove();
+			return false;
+		}
+	});
 });
