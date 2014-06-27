4737d3def91be6e1d883a37898f6b05f45b8f652
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-27 11:48:58 -0600

    "Finished JS5 - Edit in Place bonus 1"

--- exercises/week2/js5-edit-in-place/bonus1/js/main.js
+++ exercises/week2/js5-edit-in-place/bonus1/js/main.js
@@ -1,3 +1,23 @@
+// Variable declaration
+
+
+// Callback function declaration
+function test(response, newValue) {
+	$(this).text(newValue);
+}
+
+// Function declaration
+
+
+// Event handlers
 $(document).on('ready', function() {
-  
+
+	$.fn.editable.defaults.mode = 'inline';
+
+	$(".editable").editable({
+		type: "text",
+		title: "Cool stuff",
+		success: test
+	});
+
 });
