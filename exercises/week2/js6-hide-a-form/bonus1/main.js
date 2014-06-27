2a66b30ec4472272282d63207b310a1b730fb44e
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-27 16:25:26 -0600

    'Finished JS6 - Hide a Form Bonus 1'

--- exercises/week2/js6-hide-a-form/bonus1/main.js
+++ exercises/week2/js6-hide-a-form/bonus1/main.js
@@ -2,60 +2,37 @@
 
 
 // Callback function declaration
-var saveProfile = function(e) {
-	e.preventDefault();
-	var editProfile = {};
-	editProfile.name = $("#edit-name").val();
-	editProfile.bio = $("#edit-bio").val();
-	editProfile.books = $("#edit-books").val();
-	editProfile.jsLibraries = $("#edit-js-libraries").val();
-	loadProfile(editProfile);
-	switchToProfile();
-};
+function switchToForm() {
+	$(".profile").hide();
+	$(this).hide();
+	$(".edit-profile").show();
+}
 
-var switchToEditProfile = function() {
-	populateEditProfileForm();
-	toggleDisplay(".edit-button");
-	toggleDisplay(".profile");
-	toggleDisplay(".edit-profile");
-};
+function switchToProfile() {
+	$(this).closest(".container").find(".edit-profile").hide();
+	$(".profile").show();
+	$(".edit-button").show();
+}
 
-var cancelEdit = function() {
-	switchToProfile();
+function saveProfileInfo() {
+	var info = $(this).val();
+	var target = $(this).data();
+	$(target.target).text(info);
+}
+
+function selectEverythingInField() {
+	$(this).select();
 }
 
 // Function declaration
-var loadProfile = function(profile) {
-	$("#name").text(profile.name);
-	$("#bio").text(profile.bio);
-	$("#books").text(profile.books);
-	$("#js-libraries").text(profile.jsLibraries);
-};
 
-var toggleDisplay = function(element) {
-	$(element).toggle();
-};
-
-var switchToProfile = function() {
-	toggleDisplay(".edit-button");
-	toggleDisplay(".profile");
-	toggleDisplay(".edit-profile");
-};
-
-var populateEditProfileForm = function() {
-	$("#edit-name").val($("#name").text());
-	$("#edit-bio").val($("#bio").text());
-	$("#edit-books").val($("#books").text());
-	$("#edit-js-libraries").val($("#js-libraries").text());
-};
 
 // Event handlers
 $(document).on('ready', function() {
   
-	$("#edit-submit").on("click", saveProfile);
-
-	$("#edit-profile").on("click", switchToEditProfile);
-
-	$("#edit-cancel").on("click", cancelEdit);
+	$(".edit-button").on("click", switchToForm);
+	$("#edit-hide").on("click", switchToProfile);
+	$(".edit").on("blur", saveProfileInfo);
+	$(".edit").on("focus", selectEverythingInField);
 
 });
