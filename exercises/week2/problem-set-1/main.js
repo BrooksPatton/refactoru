a7dbd5a45dd1fa963d14904f5655a3b4c5fb7127
Brooks Patton <scgryphon@sbcglobal.net>
2014-06-27 17:01:46 -0600

    'Finished Problem Set 1 Bonus 1'

--- exercises/week2/problem-set-1/main.js
+++ exercises/week2/problem-set-1/main.js
@@ -19,3 +19,15 @@ var wordCount = function(sentence) {
 $(document).on('ready', function() {
   
 });
+
+function primeTime(number) {
+	if(number <= 1)
+		return false;
+	else if(number === 2)
+		return true;
+	else if(number === 4)
+		return false;
+	for(var i = 3; i < number; i++) 
+		if(number % i === 0) {return false}
+	return true;
+}
