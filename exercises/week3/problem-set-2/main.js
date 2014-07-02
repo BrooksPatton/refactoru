c3cbe59fd62ed85c0900166dc624d2afcb04f016
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-02 11:42:04 -0600

    problem set 2 bonus

--- exercises/week3/problem-set-2/main.js
+++ exercises/week3/problem-set-2/main.js
@@ -1,3 +1,19 @@
+var firstReverse = function firstReverse(str) {
+	return str.split('').reverse().join('');
+};
+
+var swapCase = function swapCase(str) {
+	var newString = '';
+	for(var i = 0; i < str.length; i++) {
+		(str[i] === str[i].toUpperCase())? newString += str[i].toLowerCase(): newString += str[i].toUpperCase();
+	}
+	return newString;
+};
+
+var letterCount = function letterCount(str) {
+	return str.split(' ').sort(function(a, b) { return b.length - a.length})[0];
+}
+
 $(document).on('ready', function() {
   
 });
