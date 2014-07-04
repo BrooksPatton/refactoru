19d41b8139a1a3306a193937d7016e58a6a6b4b5
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-04 15:30:22 -0600

    User can now create a new event

--- exercises/week3/infinite-agenda/functions.js
+++ exercises/week3/infinite-agenda/functions.js
@@ -18,11 +18,12 @@ var calendar = (function namespace() {
 			var userData = _.where(data, {date: currentDay});
 			if(userData) {
 				userData.forEach(function(item) {
-					day.append('<p>' + item.description + '</p>');
+					day.append('<div class="event"><p>' + item.description + '</p>');
 					day.append('<p>' + item.time + '</p>');
-					day.append('<p>' + item.location + '</p>');
+					day.append('<p>' + item.location + '</p></div>');
 			});
 			}
+			day.append('<div class=buttons><button class="addEntry">New Event</button></div>');
 			week.append(day);
 		}
 		date.setDate(i);
@@ -35,10 +36,37 @@ var calendar = (function namespace() {
 			$('.content').append(generateWeek());
 	}
 
+	var showAddEventForm = function showAddEventForm() {
+		var form = $('<form class="addEvent">');
+		form.append('<div><input type="text" placeholder="description" name="description"></div>');
+		form.append('<div><input type="text" placeholder="time" name="time"></div>');
+		form.append('<div><input type="text" placeholder="location" name="location"></div>');
+		form.append('<div><button class="submitNewEvent">Submit</button></div>');
+		$(this).after(form);
+		$(this).attr('disabled', 'disabled');
+	};
+
+	var submitNewEvent = function submitNewEvent(e) {
+		e.preventDefault();
+		var newEvent = {};
+		newEvent.description = $(this).closest('form').find('input[name=description]').val();
+		newEvent.time = $(this).closest('form').find('input[name=time]').val();
+		newEvent.location = $(this).closest('form').find('input[name=location]').val();
+		data.push(newEvent);
+		$(this).closest('.day').append('<div class="event"><p>' + newEvent.description + '</p>');
+		$(this).closest('.day').append('<div class="event"><p>' + newEvent.time + '</p>');
+		$(this).closest('.day').append('<div class="event"><p>' + newEvent.location + '</p>');
+		$(this).closest('.day').find('.addEntry').removeAttr('disabled');
+		$(this).closest('.day').find('.addEvent').remove();
+	}
+
 	loadData();
 
 	return {
 		generateWeek: generateWeek,
-		scrollToBottom: scrollToBottom
+		scrollToBottom: scrollToBottom,
+		showAddEventForm: showAddEventForm,
+		submitNewEvent: submitNewEvent,
+		data: data
 	};
 })();
--- exercises/week3/infinite-agenda/main.js
+++ exercises/week3/infinite-agenda/main.js
@@ -1,4 +1,6 @@
 $(document).on('ready', function() {
 	$('.content').append(calendar.generateWeek);
 	$(document).on('scroll', calendar.scrollToBottom);
+	$('.content').on('click', '.addEntry', calendar.showAddEventForm);
+	$('.content').on('click', '.submitNewEvent', calendar.submitNewEvent);
 });
