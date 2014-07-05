8ff1df6394bde263b6f817e3664706428d24d842
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-05 17:27:19 -0600

    'games played is now a thing'

--- exercises/week3/js22-starcraft-rankings/functions.js
+++ exercises/week3/js22-starcraft-rankings/functions.js
@@ -1,12 +1,28 @@
 var starcraft = (function namespace() {
 	var generateTable = function generateTable(data) {
 		insertTableShell();
+		addTotalGames(data.data);
+		addTotalGamesTitle(data.cols);
 		var dataColumns = prepDataColumns(data.cols);
 		initTable(dataColumns, data.data);
 	};
 
 	var insertTableShell = function insertTableShell() {
 		$('.container').append('<table class="display" id="starcraft"</table>');
+	};
+
+	var addTotalGames = function addTotalGames(data) {
+		for(var i = 0; i < data.length; i++) {
+			var games = _.filter(data[i], function(element) {
+				return typeof(element) === 'number';
+			});
+			var totalGames = _.reduce(games, function(memo, num) {return memo + num;}, 0);
+			data[i].push(totalGames);
+		}
+	};
+
+	var addTotalGamesTitle = function addTotalGamesTitle(data) {
+		data.push('games played');
 	};
 
 	var prepDataColumns = function prepDataColumns(items) {
