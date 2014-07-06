04fe96270e33cd7714e16ab89d4de963ef7da6b3
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-05 18:09:34 -0600

    'finished starcraft bonus'

--- exercises/week3/js22-starcraft-rankings/functions.js
+++ exercises/week3/js22-starcraft-rankings/functions.js
@@ -1,10 +1,11 @@
 var starcraft = (function namespace() {
 	var generateTable = function generateTable(data) {
 		insertTableShell();
-		addTotalGames(data.data);
+		var totalGames = addTotalGames(data.data);
 		addTotalGamesTitle(data.cols);
 		var dataColumns = prepDataColumns(data.cols);
 		initTable(dataColumns, data.data);
+		generateStatistics(data, totalGames);
 	};
 
 	var insertTableShell = function insertTableShell() {
@@ -12,13 +13,16 @@ var starcraft = (function namespace() {
 	};
 
 	var addTotalGames = function addTotalGames(data) {
+		var total = 0;
 		for(var i = 0; i < data.length; i++) {
 			var games = _.filter(data[i], function(element) {
 				return typeof(element) === 'number';
 			});
 			var totalGames = _.reduce(games, function(memo, num) {return memo + num;}, 0);
 			data[i].push(totalGames);
+			total += totalGames;
 		}
+		return total;
 	};
 
 	var addTotalGamesTitle = function addTotalGamesTitle(data) {
@@ -43,8 +47,46 @@ var starcraft = (function namespace() {
 		});
 	};
 
+	var generateStatistics = function generateStatistics(data, totalGames) {
+		var totalPlayers = getTotalPlayers(data.data);
+		var popularRace = getPopularRace(data.data);
+		displayTotalPlayers(totalPlayers);
+		displayTotalGames(totalGames);
+		displayPopularRace(popularRace);
+	};
+
+	var getTotalPlayers = function getTotalPlayers(data) {
+		return data.length;
+	};
+
+	var getPopularRace = function getPopularRace(data) {
+		var races = [];
+		var popularRace;
+		data.forEach(function(player) {
+			races.push(player[3]);
+		});
+		var gaussCollection = new gauss.Collection(races);
+		gaussCollection.mode(function(race) {
+			popularRace = race;
+		});
+		return popularRace;
+	};
+
+	var displayTotalPlayers = function displayTotalPlayers(total) {
+		$('#total-players').text(total);
+	};
+
+	var displayTotalGames = function displayTotalGames(total) {
+		$('#total-games').text(total);
+	};
+
+	var displayPopularRace = function displayPopularRace(race) {
+		$('#popular-race').text(race);
+	};
+
 	return {
 		generateTable: generateTable,
+		generateStatistics: generateStatistics
 	};
 
 })();
