2b92f0deaeedec7c77328e890ac1e74892cdf906
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-17 12:38:14 -0600

    'Commented runway.js'

--- midterm/airplane-lander/js/runway.js
+++ midterm/airplane-lander/js/runway.js
@@ -1,3 +1,7 @@
+/**
+ * The Runway constructor
+ * @param {string} size craft the css class that will determine the size of the runway
+ */
 Lander.Runway = function(size) {
 	switch (size) {
 		case 'small':
@@ -19,6 +23,9 @@ Lander.Runway = function(size) {
 	this.status = 'enabled';
 };
 
+/**
+ * Create the jQuery element of the runway
+ */
 Lander.Runway.prototype.create = function() {
 	this.el = $('<div class="runway col-md-2 col-md-offset-1">');
 	this.el.attr('id', this.size);
@@ -26,6 +33,11 @@ Lander.Runway.prototype.create = function() {
 	this.el.append('<img src="images/runway.png">');
 };
 
+/**
+ * The plane is requesting the position of the runway so that it knows where the land
+ * @param  {string} planeSize The size of the plane so the runway can tell the plane what offset it needs to be to land relatively centered
+ * @return {object}           The position of the runway for the specific plane
+ */
 Lander.Runway.prototype.getPosition = function(planeSize) {
 	var modifier = 0;
 	switch (planeSize) {
@@ -45,10 +57,18 @@ Lander.Runway.prototype.getPosition = function(planeSize) {
 	return this.position;
 };
 
+/**
+ * Enable the runway for use
+ * @param  {Function} callback The callback function to be called when the User clicks on the runway
+ */
 Lander.Runway.prototype.enable = function (callback) {
 	this.el.bind('click', callback);
 };
 
+/**
+ * Disable the runway when it is in use so that the Player cannot click on it
+ * @param  {Function} callback The function that the click event handler had been set to.
+ */
 Lander.Runway.prototype.disable = function(callback) {
 	this.el.unbind('click', callback);
 };
