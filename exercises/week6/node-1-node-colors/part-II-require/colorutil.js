f758b1294b47c197967dcc7203e9969d179deb76
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-21 16:10:00 -0600

    'Node I: Node Colors - Bonus I'

--- exercises/week6/node-1-node-colors/part-II-require/colorutil.js
+++ exercises/week6/node-1-node-colors/part-II-require/colorutil.js
@@ -2,6 +2,12 @@ var luminosity = function(r, g, b) {
 	return 0.2126*r + 0.7152*g + 0.0722*b;
 };
 
+var darken = function(r, g, b) {
+	var result = [r * 0.8, g * 0.8, b * 0.8];
+	return result.join(' ');
+};
+
 module.exports = {
-	luminosity: luminosity
+	luminosity: luminosity,
+	darken: darken
 };
