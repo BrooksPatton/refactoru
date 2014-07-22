67b21d6f534322f7d78d770612294e7dd8d86786
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-22 14:39:09 -0600

    'Problem set 4'

--- exercises/week6/problem-set-4/main.js
+++ exercises/week6/problem-set-4/main.js
@@ -21,11 +21,25 @@ var addNumbers = function(str) {
 	console.log(sum);
 };
 
-addNumbers("88Hello 3World!");
-addNumbers("55Hello");
-addNumbers("5Hello 5");
+// addNumbers("88Hello 3World!");
+// addNumbers("55Hello");
+// addNumbers("5Hello 5");
 
 // Write a function longestWord that takes a single string parameter and returns the largest word in the string. If there are two or more words that are the same length, it returns the first word from the string with that length. Ignore punctuation and assume the input sentence will not be empty.
+var longestWord = function(str) {
+	var words = str.split(" ");
+	var longestWord = words.shift();
+	for(var i = 0; i < words.length; i++) {
+		if(words[i].length > longestWord.length) {
+			longestWord = words[i];
+		}
+	}
+	console.log(longestWord);
+};
+
+longestWord('If there are');
+longestWord('Ignore punctuation and assume the input sentence');
+longestWord('one two');
 
 // Bonus:
 // Write a function averageStringNumbers that takes a single string parameter and searches for all the numbers in the string, adds them together, then returns that final number divided by the total amount of letters in the string. For example: if str is "Hello6 9World 2, Nic8e D7ay!" the output should be 2. First if you add up all the numbers, 6 + 9 + 2 + 8 + 7 you get 32. Then there are 17 letters in the string. 32 / 17 = 1.882, and the final answer should be rounded to the nearest whole number, so the answer is 2. Only single digit numbers separated by spaces will be used throughout the whole string (So this won't ever be the case: hello44444 world). Each string will also have at least one letter.
