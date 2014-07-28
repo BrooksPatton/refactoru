5fc6e3ddfb972b495edc591eba402b206ce42c7f
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-28 11:46:39 -0600

    'Node8: online job application part 1 completed'

--- exercises/week7/node8-online-job-application/app.js
+++ exercises/week7/node8-online-job-application/app.js
@@ -20,7 +20,12 @@ app.get('/applicants', function(req, res){
 app.post('/applicant', function(req, res){
 	// Here is where you need to get the data
 	// from the post body and store it in the database
-	res.send('No funciona');
+	console.log(req.body);
+	res.redirect('/success');
+});
+
+app.get('/success', function(req, res) {
+	res.render('success');
 });
 
 var server = app.listen(8441, function() {
