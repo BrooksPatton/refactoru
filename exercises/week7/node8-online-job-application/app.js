d3f2182ecda2887486f95c72e432685077ef1ac7
Brooks Patton <brooks-patton@sbcglobal.net>
2014-07-28 12:07:40 -0600

    'Node8: Online job application part 3 completed'

--- exercises/week7/node8-online-job-application/app.js
+++ exercises/week7/node8-online-job-application/app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
+
+mongoose.connect('mongodb://localhost/awesomeJS');
 
 app.get('/', function(req, res) {
 	res.render('index');
@@ -20,6 +23,22 @@ app.get('/applicants', function(req, res){
 app.post('/applicant', function(req, res){
 	// Here is where you need to get the data
 	// from the post body and store it in the database
+	var userResponse = req.body;
+	var Applicant = mongoose.model('Applicant', {
+		'name': String,
+		'bio': String,
+		'skills': String,
+		'years': Number,
+		'why': String
+	});
+	var applicant = new Applicant({
+		name: userResponse.name,
+		bio: userResponse.bio,
+		skills: userResponse.skills,
+		years: userResponse.years,
+		why: userResponse.why
+	});
+	applicant.save();
 	console.log(req.body);
 	res.redirect('/success');
 });
